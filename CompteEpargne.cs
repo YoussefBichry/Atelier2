b069e33faa4a5663da5959f073567b66ac37e0c9
YoussefBichry <75500068+YoussefBichry@users.noreply.github.com>
2021-03-22 11:09:23 +0100

    Add files via upload

---
+++ CompteEpargne.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compte
+{
+    class CompteEpargne :Compte
+    {
+        private double TauxInteret;
+        public CompteEpargne(Client titu,MAD solde,MAD max) : base(titu, solde, max)
+        {
+            double taux = double.Parse(Console.ReadLine());
+            while(taux<=0 || taux > 100)
+            {
+                Console.WriteLine("donnez le taux");
+                taux = double.Parse(Console.ReadLine());
+            }
+            this.TauxInteret = taux;
+        }
+        public void calculInteret()
+        {
+           solde += solde * TauxInteret / 100;
+        }
+
+    }
+}
---
+++ Operation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compte
+{
+    class Operation
+    {
+        private int numero;
+        private DateTime date = DateTime.Now;
+        private MAD montant;
+        private string libelle;
+        public void afficherOpert()
+        {
+            Console.WriteLine(date + "| n°" + numero + "|-" + montant + "MAD");
+        } 
+    }
+}
